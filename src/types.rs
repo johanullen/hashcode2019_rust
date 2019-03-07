a10f834c763cb8fd14c6481367c0abe9aa85d035
Johan Ullen <johanullen@hotmail.com>
2019-03-07 23:58:31 +0100

    removed stupid slides enum

--- src/types.rs
+++ src/types.rs
@@ -11,33 +11,3 @@ pub enum Pic {
 }
 pub type Pair = (Pic, Pic);
 pub type Pics = Vec<Pic>;
-pub enum Slides {
-    ONE(Pic),
-    MANY(Pics),
-}
-
-
-impl Add for Slides {
-    type Output = Pics;
-    fn add (self,  rhs:Slides) -> Pics {
-        match (self, rhs) {
-            (Slides::ONE(a), Slides::ONE(b)) => vec![a, b],
-            (Slides::ONE(a), Slides::MANY(b)) => {
-                let mut a = vec![a];
-                a.append(&mut b.to_vec());
-                a
-            },
-            (Slides::MANY(a), Slides::ONE(b)) => {
-                let mut a = a.to_vec();
-                a.push(b);
-                a
-            }
-            (Slides::MANY(a), Slides::MANY(b)) => {
-                let mut a = a.to_vec();
-                a.append(&mut b.to_vec());
-                a
-            }
-
-        }
-    }
-}
