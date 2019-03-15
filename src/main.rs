50275eb4a9c8a0b0a523f26cbadd29f198af42e8
Johan Ullen <johanullen@hotmail.com>
2019-03-15 20:52:14 +0100

    refactor: wrapper method for running read and algs with pritns and timers

--- src/main.rs
+++ src/main.rs
@@ -5,61 +5,66 @@
 mod greedy2;
 mod io;
 mod types;
+use crate::types::Pics;
 use greedy2::iterative_greedy;
 use io::read;
 use std::time::Instant;
 use types::Score;
 
+fn run_alg(alg: &Fn(&Pics) -> Pics, pics: &Pics, name: &str, print_pics: bool) -> Pics {
+    let interm = Instant::now();
+    let pics = alg(pics);
+    let end = interm.elapsed();
+    println!("\t{:?}", name);
+    if print_pics {
+        println!("{:?}", pics);
+    }
+    println!("\tscore all {:?}", pics.score());
+    println!(
+        "\ttime: {:.4}s",
+        end.as_secs() as f64 + end.subsec_nanos() as f64 * 1e-9
+    );
+    println!("\t-----------------------------------");
+    pics
+}
+
+fn run_read(filename: &str, print_pics: bool) -> Pics {
+    let start = Instant::now();
+    let pics = read(filename);
+    let end = start.elapsed();
+    println!("read {:?}", filename);
+    if print_pics {
+        println!("{:?}", pics);
+    }
+    println!("\tscore all {:?}", pics.score());
+    println!(
+        "\ttime: {:.4}s",
+        end.as_secs() as f64 + end.subsec_nanos() as f64 * 1e-9
+    );
+    println!("\t-----------------------------------");
+    pics
+}
+
 fn main() {
+    let print_pics = false;
     let start = Instant::now();
     // let filename = "data/a_example.txt";
     // let filename = "data/b_lovely_landscapes.txt";
     // let filename = "data/c_memorable_moments.txt";
     let filenames = vec![
         "data/a_example.txt",
-        "data/b_lovely_landscapes.txt",
-        "data/c_memorable_moments.txt",
+        // "data/b_lovely_landscapes.txt",
+        // "data/c_memorable_moments.txt",
     ];
     println!("*******************************************");
     for filename in filenames {
-        let ds_timer = Instant::now();
         let interm = Instant::now();
-        let pics = read(filename);
-        let end = interm.elapsed();
-        println!("read {:?}", filename);
-        // println!("{:?}", pics);
-        println!("\tscore all {:?}", pics.score());
-        println!(
-            "\ttime: {:.4}s",
-            end.as_secs() as f64 + end.subsec_nanos() as f64 * 1e-9
-        );
-        println!("\t-----------------------------------");
 
-        let interm = Instant::now();
-        let pics = iterative_greedy(&pics);
-        let end = interm.elapsed();
-        println!("\titerative_greedy");
-        // println!("{:?}", pics);
-        println!("\tscore all {:?}", pics.score());
-        println!(
-            "\ttime: {:.4}s",
-            end.as_secs() as f64 + end.subsec_nanos() as f64 * 1e-9
-        );
-        println!("\t-----------------------------------");
+        let pics = run_read(filename, print_pics);
+        let pics = run_alg(&iterative_greedy, &pics, "iterative_greedy", print_pics);
+        let pics = run_alg(&iterative_greedy, &pics, "iterative_greedy 2", print_pics);
 
-        let interm = Instant::now();
-        let pics = iterative_greedy(&pics);
         let end = interm.elapsed();
-        println!("\titerative_greedy #2");
-        // println!("{:?}", pics);
-        println!("\tscore all {:?}", pics.score());
-        println!(
-            "\ttime: {:.4}s",
-            end.as_secs() as f64 + end.subsec_nanos() as f64 * 1e-9
-        );
-        println!("\t-----------------------------------");
-
-        let end = ds_timer.elapsed();
         println!(
             "time: {:.4}s",
             end.as_secs() as f64 + end.subsec_nanos() as f64 * 1e-9
